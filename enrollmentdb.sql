d32acd5924e2776b0984a7690daa25ba64647f25
julianne060701 <fernandez.226952@gensan.sti.edu.ph>
2023-06-11 18:45:11 +0800

    sql

--- enrollmentdb.sql
+++ enrollmentdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2023 at 06:01 AM
+-- Generation Time: Jun 11, 2023 at 09:36 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,7 +42,14 @@ CREATE TABLE `tbl_contactinfo` (
 --
 
 INSERT INTO `tbl_contactinfo` (`contact_id`, `userinfo_id`, `email`, `contact_num`, `city`, `barangay`, `street`) VALUES
-(1, 1, 'jvburdo@gmail.com', 1231231, 'Gensan', 'conel', 'guadalupe');
+(1, 1, 'jvburdo@gmail.com', 1231231, 'Gensan', 'conel', 'guadalupe'),
+(3, 4, 'mynftacc269@gmail.com', 9084082735, 'General Santos City', 'Bule', 'Gensanville'),
+(4, 5, 'jvburdo@gmail.com', 1231231, 'Gensan', 'conel', 'guadalupe'),
+(5, 6, 'jvburdo@gmail.com', 1231231, 'Gensan', 'conel', 'guadalupe'),
+(6, 7, 'eim@gmail.com', 9087823721, 'General Santos City', 'conel', 'guadalupe'),
+(7, 8, 'fbs@gmail.com', 9087823721, 'General Santos City', 'Barangay', 'guadalupe'),
+(8, 9, 'smaw@gmai.com', 9087823721, 'smaw', 'smaw', 'smaw'),
+(9, 10, 'ICT@gmail.com', 9087823721, 'ICT', 'ICT', 'ICT');
 
 -- --------------------------------------------------------
 
@@ -66,7 +73,14 @@ CREATE TABLE `tbl_enrollment` (
 --
 
 INSERT INTO `tbl_enrollment` (`enrollment_id`, `userinfo_id`, `admit_type`, `grade`, `program`, `term`, `lrn`, `lsa`) VALUES
-(1, 1, 'new', '11', 'abm', '1st', 1123123121, '');
+(1, 1, 'new', '12', 'abm', '1st', 2, 'For transferee only'),
+(3, 4, '', '', '', '', 0, ''),
+(4, 5, 'old', '12', 'humms', '2nd', 2, 'For transferee only'),
+(5, 6, 'old', '11', 'stem', '1st', 2, 'For transferee only'),
+(6, 7, 'new', '11', 'eim', '1st', 1231123, ''),
+(7, 8, 'new', '11', 'fbs', '1st', 123112321, ''),
+(8, 9, 'new', '11', 'smaw', '1st', 22222123, ''),
+(9, 10, 'transferee', '12', 'ict', '2nd', 231123122, '');
 
 -- --------------------------------------------------------
 
@@ -88,8 +102,8 @@ CREATE TABLE `tbl_subjects` (
 --
 
 INSERT INTO `tbl_subjects` (`subject_id`, `strand`, `grade`, `subjects`, `schedules`, `status`) VALUES
-(1, 'abm', '11', 'Financial Management', '10:00:00', 1),
-(2, 'humss', '12', 'Humanitas', '13:30:00', 1);
+(1, 'stem', '11', 'Technology', '21:09:00', 1),
+(2, 'abm', '11', 'Financial Management', '14:11:00', 1);
 
 -- --------------------------------------------------------
 
@@ -109,7 +123,9 @@ CREATE TABLE `tbl_usercredentials` (
 --
 
 INSERT INTO `tbl_usercredentials` (`usercredentials_id`, `userinfo_id`, `username`, `password`) VALUES
-(1, 1, 'RyanXD', '$2y$10$axEHvL4AyXvRWW7w8QGwMOG1SsHnwXaGlETYXhXNo4cf/NuU/jX2m');
+(1, 1, '', '$2y$10$WxbPafDsfAyV2KEmtEi1P.QiP11otU1BVGYWVYkI/ve8obrgAL29e'),
+(3, 4, 'elmeradmin', '$2y$10$V2V65sAcge3cqeuf0wvqH.JDvJgRdZGhqCrFLue6cYHBItF0Nhuma'),
+(4, 5, '', '$2y$10$3ryTSw67j4.u90zr9UmK6eOfKvdrVedg43W8OxwTWvRE07fd6BL7u');
 
 -- --------------------------------------------------------
 
@@ -133,7 +149,14 @@ CREATE TABLE `tbl_userinfo` (
 --
 
 INSERT INTO `tbl_userinfo` (`user_id`, `firstname`, `middlename`, `lastname`, `suffix`, `gender`, `birthday`, `age`) VALUES
-(1, 'Ryan', 'Cutie', 'laroco', 'XD', 'male', '2023-06-03', 22);
+(1, 'Ryan', 'Cuties', 'larocos', 'XDs', '', '2023-06-03', 22),
+(4, 'Elmer', '', 'Varquez', '', 'male', '2001-06-06', 22),
+(5, 'Ryan', 'Cutie', 'laroco', 'XD', '', '2023-06-03', 22),
+(6, 'Ryan', 'Cutie', 'laroco', 'XD', '', '2023-06-03', 22),
+(7, 'EIM', '', 'Student', '', 'female', '2011-02-22', 22),
+(8, 'FPB', '', 'Student', '', 'male', '2001-02-12', 22),
+(9, 'SMAW', '', 'Student', '', 'male', '2007-06-06', 22),
+(10, 'ICT', '', 'Student', '', 'male', '2002-02-22', 22);
 
 -- --------------------------------------------------------
 
@@ -152,7 +175,14 @@ CREATE TABLE `tbl_user_level` (
 --
 
 INSERT INTO `tbl_user_level` (`user_level_id`, `userinfo_id`, `level`) VALUES
-(1, 1, 'STUDENT');
+(1, 1, 'STUDENT'),
+(4, 4, 'ADMIN'),
+(5, 5, 'STUDENT'),
+(6, 6, 'STUDENT'),
+(7, 7, 'STUDENT'),
+(8, 8, 'STUDENT'),
+(9, 9, 'STUDENT'),
+(10, 10, 'STUDENT');
 
 -- --------------------------------------------------------
 
@@ -171,7 +201,14 @@ CREATE TABLE `tbl_user_status` (
 --
 
 INSERT INTO `tbl_user_status` (`status_id`, `userinfo_id`, `status`) VALUES
-(1, 1, 1);
+(1, 1, 1),
+(4, 4, 1),
+(5, 5, 1),
+(6, 6, 1),
+(7, 7, 1),
+(8, 8, 1),
+(9, 9, 1),
+(10, 10, 1);
 
 --
 -- Indexes for dumped tables
@@ -232,13 +269,13 @@ ALTER TABLE `tbl_user_status`
 -- AUTO_INCREMENT for table `tbl_contactinfo`
 --
 ALTER TABLE `tbl_contactinfo`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_enrollment`
 --
 ALTER TABLE `tbl_enrollment`
-  MODIFY `enrollment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `enrollment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_subjects`
@@ -250,25 +287,25 @@ ALTER TABLE `tbl_subjects`
 -- AUTO_INCREMENT for table `tbl_usercredentials`
 --
 ALTER TABLE `tbl_usercredentials`
-  MODIFY `usercredentials_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `usercredentials_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_userinfo`
 --
 ALTER TABLE `tbl_userinfo`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_user_level`
 --
 ALTER TABLE `tbl_user_level`
-  MODIFY `user_level_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_level_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_user_status`
 --
 ALTER TABLE `tbl_user_status`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
